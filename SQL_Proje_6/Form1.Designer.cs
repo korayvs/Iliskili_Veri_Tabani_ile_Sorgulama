3fb3ee88a85d878360025eef0598dec7d0117ac2
Koray <Koray@DESKTOP-F1A12T8>
2024-12-21 19:01:47 +0300

    Add project files.

---
+++ SQL_Proje_6/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace SQL_Proje_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.CmbUrun = new System.Windows.Forms.ComboBox();
+            this.CmbMusteri = new System.Windows.Forms.ComboBox();
+            this.CmbPersonel = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TxtFiyat = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(961, 218);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // CmbUrun
+            // 
+            this.CmbUrun.FormattingEnabled = true;
+            this.CmbUrun.Location = new System.Drawing.Point(146, 79);
+            this.CmbUrun.Name = "CmbUrun";
+            this.CmbUrun.Size = new System.Drawing.Size(193, 24);
+            this.CmbUrun.TabIndex = 1;
+            // 
+            // CmbMusteri
+            // 
+            this.CmbMusteri.FormattingEnabled = true;
+            this.CmbMusteri.Location = new System.Drawing.Point(146, 109);
+            this.CmbMusteri.Name = "CmbMusteri";
+            this.CmbMusteri.Size = new System.Drawing.Size(193, 24);
+            this.CmbMusteri.TabIndex = 2;
+            // 
+            // CmbPersonel
+            // 
+            this.CmbPersonel.FormattingEnabled = true;
+            this.CmbPersonel.Location = new System.Drawing.Point(146, 139);
+            this.CmbPersonel.Name = "CmbPersonel";
+            this.CmbPersonel.Size = new System.Drawing.Size(193, 24);
+            this.CmbPersonel.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.TxtFiyat);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.CmbMusteri);
+            this.groupBox1.Controls.Add(this.CmbUrun);
+            this.groupBox1.Controls.Add(this.CmbPersonel);
+            this.groupBox1.Location = new System.Drawing.Point(282, 227);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(403, 266);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(92, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Ürün:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(75, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Müşteri:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(65, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Personel:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(93, 173);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Fiyat:";
+            // 
+            // TxtFiyat
+            // 
+            this.TxtFiyat.Location = new System.Drawing.Point(146, 169);
+            this.TxtFiyat.Name = "TxtFiyat";
+            this.TxtFiyat.Size = new System.Drawing.Size(193, 22);
+            this.TxtFiyat.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(174, 197);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 33);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Satış Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(976, 505);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox CmbUrun;
+        private System.Windows.Forms.ComboBox CmbMusteri;
+        private System.Windows.Forms.ComboBox CmbPersonel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox TxtFiyat;
+    }
+}
+
---
+++ SQL_Proje_6/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SQL_Proje_6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection baglanti = new SqlConnection(@"Data Source=DESKTOP-F1A12T8\KORAY;Initial Catalog=Proje6;Integrated Security=True");
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlDataAdapter da = new SqlDataAdapter("Execute Proje6", baglanti);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+            SqlCommand urun = new SqlCommand("Select * From URUNLER", baglanti);
+            SqlDataAdapter da1 = new SqlDataAdapter(urun);
+            DataTable dt1 = new DataTable();
+            da1.Fill(dt1);
+            CmbUrun.ValueMember = "ID";           
+            CmbUrun.DisplayMember = "AD";
+            CmbUrun.DataSource = dt1;
+            CmbUrun.SelectedIndex = -1;
+
+            SqlCommand musteri = new SqlCommand("Select * From MUSTERILER", baglanti);
+            SqlDataAdapter da2 = new SqlDataAdapter(musteri);
+            DataTable dt2 = new DataTable();
+            da2.Fill(dt2);
+            CmbMusteri.ValueMember = "ID";
+            CmbMusteri.DisplayMember = "ADSOYAD";
+            CmbMusteri.DataSource = dt2;
+            CmbMusteri.SelectedIndex = -1;
+
+            SqlCommand personel = new SqlCommand("Select * From PERSONELLER", baglanti);
+            SqlDataAdapter da3 = new SqlDataAdapter(personel);
+            DataTable dt3 = new DataTable();
+            da3.Fill(dt3);
+            CmbPersonel.ValueMember = "ID";
+            CmbPersonel.DisplayMember = "AD";
+            CmbPersonel.DataSource = dt3;
+            CmbPersonel.SelectedIndex = -1;            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("Insert Into HAREKETLER (URUN, MUSTERI, PERSONEL, FIYAT) Values (@p1, @p2, @p3, @p4)", baglanti);
+            komut.Parameters.AddWithValue("@p1", CmbUrun.SelectedValue);
+            komut.Parameters.AddWithValue("@p2", CmbMusteri.SelectedValue);
+            komut.Parameters.AddWithValue("@p3", CmbPersonel.SelectedValue);
+            komut.Parameters.AddWithValue("@p4", TxtFiyat.Text);
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+        }
+    }
+}
